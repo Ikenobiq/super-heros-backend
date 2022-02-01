021757b57d5311e190cbb09510a2af71f9cf7781
Andrii Ivanichenko <ikenobiq@gmail.com>
2022-02-02 01:40:50 +0200

    fix

--- controllers/superheroes/getAll.js
+++ controllers/superheroes/getAll.js
@@ -1,11 +1,23 @@
 const { Superhero } = require("../../models/superheroes");
 
 const getAll = async (req, res) => {
-  const result = await Superhero.find({});
+  const page = parseInt(req.query.page);
+  const pageSize = parseInt(req.query.pageSize);
+  const skipIndex = (page - 1) * pageSize;
+
+  const totalCount = (await Superhero.find().exec()).length;
+
+  const result = await Superhero.find()
+    .sort({ _id: 1 })
+    .limit(pageSize)
+    .skip(skipIndex)
+    .exec();
+
   res.json({
     status: "success",
     code: 200,
     data: {
+      totalCount,
       result,
     },
   });
